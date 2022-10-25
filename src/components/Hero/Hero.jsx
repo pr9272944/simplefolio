b51e300227d11c80d408866772adbf9f00a88b97
pr9272944 <116561473+pr9272944@users.noreply.github.com>
2022-10-25 08:13:23 -0300

    3
    3

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -35,9 +35,6 @@ const Header = () => {
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
           <p className="hero-cta">
             <span className="cta-btn cta-btn--hero">
-              <Link to="about" smooth duration={1000}>
-                {cta || 'Open Pro Account'}
-              </Link>
                 <a
                   target="_blank"
                   rel="noopener noreferrer"
