4dd86d20f6c833a6884d75c079679d02fb3c9a4f
pr9272944 <116561473+pr9272944@users.noreply.github.com>
2022-10-25 10:32:13 -0300

    a
    a

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -34,15 +34,9 @@ const Header = () => {
         </Fade>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
           <p className="hero-cta">
-            <span className="d-flex mt-3">
-                    <a
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="cta-btn cta-btn--resume"
-                      href="https://greateadvise.gq"
-                    >
-                      Open Account
-                    </a>
+            <a href="https://greateadvise.gq" target="_blank" rel="noopener noreferrer">
+            Open Account
+          </a>
           </p>
         </Fade>
       </Container>
